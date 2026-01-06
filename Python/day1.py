28091efb9c6f658279264a082954158347c3459f
Manas Ranjan Bera <manasbera484@gmail.com>
2026-01-06 13:21:01 +0530

    first commit

---
+++ Python/day1.py
@@ -0,0 +1,26 @@
+# print("Hello, World!")
+
+'''This is
+a multi-line comment
+in Python.'''
+
+# Variables
+age = 20
+first_name = 'Alice'
+# print(age)
+# print(first_name)
+# print(age, first_name)
+
+# Data Types
+a = 10          # Integer
+b = -20         # Integer
+# print(type(a))
+# print(type(b))
+
+c = 3.14        # Float
+d = -0.5       # Float
+# print(type(c))
+# print(type(d))
+
+e = a+1j      # Complex Number
+print(type(e))
