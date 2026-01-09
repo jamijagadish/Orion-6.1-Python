21401363caab6fefc3991e06a196ebf8e516067b
Manas Ranjan Bera <manasbera484@gmail.com>
2026-01-09 14:49:14 +0530

    Notes

---
+++ Python/day4.py
@@ -0,0 +1,61 @@
+# For loop
+# for i in range():
+#     statement
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(2,10):
+#     print(i)
+
+# for i in range(2,10,3):
+#     print(i)
+
+# arr = [1,2,3,4,5]
+# for i in arr:
+#     print(i)
+
+# s1 = "Hello Everyone"
+# for i in s1:
+#     print(i)
+
+# for i in range(1,50,2):
+#     print(i)
+
+# for i in range(50):
+#     if i % 2 == 0:
+#         print(i)
+
+# While Loop
+# initialization
+# while condition:
+#     statement
+#     increment/decrement
+
+i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# j = 50
+# while j >=10:
+#     print(j)
+#     j -=1
+
+# for i in range(50,10,-1):
+#   print(i)
+
+# Nested Loop
+x = 0
+# for i in range(5): 
+#     for j in range(5):
+#         for k in range(5):
+#             print(i,j,k)
+#             x += 1
+
+# print(x)
+
+for i in range(4):
+    while i < 4:
+        print(i)
+        i += 1
